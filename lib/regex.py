2ee466254fc81328178c5366bcd4d828d1cdebc3
Joee <gachaujoe@gmail.com>
2024-10-16 10:37:19 +0300

    complete

--- lib/regex.py
+++ lib/regex.py
@@ -1,5 +1,32 @@
+# import re
+
+# my_pattern = r""
+# my_regex = re.compile(my_pattern)
+# import re
+
+# # Regular expression to match the test cases
+# my_regex = re.compile(r"^[A-Za-z\s',?.!]+$")
+
+# # Test string for the findall test case
+# FINDALL_STRING = '''
+# It's such a lovely day today.
+# Some weather we're having today, huh?
+# Maybe today's just not my day.
+# '''
+
 import re
 
-my_pattern = r""
-my_regex = re.compile(my_pattern)
+# Adjust the regular expression to capture each sentence specifically
+my_regex = re.compile(r"(It's such a lovely day today\.|Some weather we're having today, huh\?|Maybe today's just not my day\.)")
+
+# Test string for the findall test case
+FINDALL_STRING = '''
+It's such a lovely day today.
+Where'd all the time go?
+Some weather we're having today, huh?
+Tomorrow never knows!
+Maybe today's just not my day.
+It's clobbering time!
+'''
+
 
